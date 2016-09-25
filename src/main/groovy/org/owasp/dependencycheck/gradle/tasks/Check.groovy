18faa43ed314e23d6b08f50f929328ce9bcdc529
Stefan Neuhaus <stefan@stefanneuhaus.org>
2016-09-25 14:07:05 +0200

    Extend test configuration detection to better address Android builds A configuration is considered a test configuration if and only if any of the following conditions holds: - the name of the configuration or any of its parent configurations equals 'testCompile' - the name of the configuration or any of its parent configurations equals 'androidTestCompile' - the configuration name starts with 'test' - the configuration name starts with 'androidTest'

--- src/main/groovy/org/owasp/dependencycheck/gradle/tasks/Check.groovy
+++ src/main/groovy/org/owasp/dependencycheck/gradle/tasks/Check.groovy
@@ -306,14 +306,20 @@ class Check extends DefaultTask {
 
     /**
      * Checks whether a configuration is considered to be a test configuration in order to skip it.
-     * This boils down to check whether a configuration extends (transitively) from {@code testCompile}.
+     * A configuration is considered a test configuration if and only if any of the following conditions holds:
+     * <ul>
+     *     <li>the name of the configuration or any of its parent configurations equals 'testCompile'</li>
+     *     <li>the name of the configuration or any of its parent configurations equals 'androidTestCompile'</li>
+     *     <li>the configuration name starts with 'test'</li>
+     *     <li>the configuration name starts with 'androidTest'</li>
+     * </ul>
      */
     def isTestConfiguration(configuration) {
-        def isTestConfiguration = false
+        def isTestConfiguration = configuration.name.startsWith("test") || configuration.name.startsWith("androidTest")
         configuration.hierarchy.each {
-            isTestConfiguration |= it.name.equals("testCompile")
+            isTestConfiguration |= (it.name == "testCompile" || it.name == "androidTestCompile")
         }
-        logger.debug("'${configuration.name}' is a test configuration: ${isTestConfiguration}")
+        logger.info("'${configuration.name}' is considered a test configuration: ${isTestConfiguration}")
         isTestConfiguration
     }
 }
