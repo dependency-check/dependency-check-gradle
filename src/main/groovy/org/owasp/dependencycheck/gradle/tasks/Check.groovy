f57ff499a63ab2aa1672d38f528512592a2bd017
Stefan Neuhaus <dr.stefan.neuhaus@gmail.com>
2017-02-05 15:58:29 +0100

    Cleanup: remove unused imports

--- src/main/groovy/org/owasp/dependencycheck/gradle/tasks/Check.groovy
+++ src/main/groovy/org/owasp/dependencycheck/gradle/tasks/Check.groovy
@@ -21,13 +21,8 @@ package org.owasp.dependencycheck.gradle.tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.InvalidUserDataException
-import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ResolvedArtifact
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.OutputDirectories
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.owasp.dependencycheck.Engine
 import org.owasp.dependencycheck.data.nvdcve.CveDB
