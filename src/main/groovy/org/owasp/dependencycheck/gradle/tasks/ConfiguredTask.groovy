aa6e9d078370378167c2d2a921d4fdab0229cdb4
Hans Aikema <aikebah-github@aikebah.net>
2024-07-27 12:59:49 +0200

    feat: Allow configurable resultsPerPage in Gradle plugin (#399)

--- src/main/groovy/org/owasp/dependencycheck/gradle/tasks/ConfiguredTask.groovy
+++ src/main/groovy/org/owasp/dependencycheck/gradle/tasks/ConfiguredTask.groovy
@@ -91,6 +91,7 @@ abstract class ConfiguredTask extends DefaultTask {
         settings.setStringIfNotEmpty(NVD_API_KEY, config.nvd.apiKey)
         settings.setStringIfNotEmpty(NVD_API_ENDPOINT, config.nvd.endpoint)
         settings.setIntIfNotNull(NVD_API_DELAY, config.nvd.delay)
+        settings.setIntIfNotNull(NVD_API_RESULTS_PER_PAGE, config.nvd.resultsPerPage)
         settings.setIntIfNotNull(NVD_API_MAX_RETRY_COUNT, config.nvd.maxRetryCount)
         settings.setIntIfNotNull(NVD_API_VALID_FOR_HOURS, config.nvd.validForHours);
 
