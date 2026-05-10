991e867bc3d224cc8794231eee93aafb0fddce66
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-05-10 23:58:35 +0800

    build(deps): bump Gradle version to 8.14.5
    Also allow testing with Java 25; should have sufficient compatibility now.
    
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- src/test/groovy/org/owasp/dependencycheck/gradle/GradleTestVersion.groovy
+++ src/test/groovy/org/owasp/dependencycheck/gradle/GradleTestVersion.groovy
@@ -12,7 +12,7 @@ class GradleTestVersion {
      */
     final static def supportedVersions = [
             new GradleTestVersion(version: '7.6.6',  minJdk: 8,  maxJdk: 19),
-            new GradleTestVersion(version: '8.14.4', minJdk: 8,  maxJdk: 24),
+            new GradleTestVersion(version: '8.14.5', minJdk: 8,  maxJdk: 25),
             new GradleTestVersion(version: '9.5.0',  minJdk: 17, maxJdk: 26),
     ]
 
